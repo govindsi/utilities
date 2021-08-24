e4d98a1f701d379e7e912d6aac487d60b1315661
Govind Singh <govind.sk85@gmail.com>
2021-08-24 18:03:25 +0530

    utils: Add ath10k spectral scan script
    Add ath10k spectral scan script to capture spectral bin
    (FFT samples) using QCA6174 module.
    
    sudo spectral_scan.sh -i wlan0
    
    Signed-off-by: Govind Singh <govind.sk85@gmail.com>

---
+++ scripts/spectral_scan.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+help()
+{
+ echo "sudo spectral_scan.sh -i wlan0"
+}
+
+while getopts ":i:h" option; do
+  case $option in
+    i)
+      if=${OPTARG};;
+        h) help
+           exit;;
+  esac
+done
+
+echo $if
+phy_if=$(iw $if info | gawk '/wiphy/ {print "phy" $2}')
+echo $phy_if
+#set background scan and trigger spectral scan in FW
+echo background > /sys/kernel/debug/ieee80211/$phy_if/ath10k/spectral_scan_ctl
+echo trigger > /sys/kernel/debug/ieee80211/$phy_if/ath10k/spectral_scan_ctl
+#Initiate scan
+iw dev $if scan
+#disable spectral scan
+echo disable > /sys/kernel/debug/ieee80211/$phy_if/ath10k/spectral_scan_ctl
+#store spectral scan output samples
+cat /sys/kernel/debug/ieee80211/$phy_if/ath10k/spectral_scan0  > /var/log/spectral_samples
